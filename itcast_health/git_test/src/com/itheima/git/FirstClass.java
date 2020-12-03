89f610c2888014eb57996e0cb633c86e1710b07d
zhangsan <zhangsan@163.com>
2020-12-03 20:40:48 +0800

    修改FirstClass

--- itcast_health/git_test/src/com/itheima/git/FirstClass.java
+++ itcast_health/git_test/src/com/itheima/git/FirstClass.java
@@ -8,12 +8,13 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
-
+        //hello，test
     }
 
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
         System.out.println(i);
     }
 }
